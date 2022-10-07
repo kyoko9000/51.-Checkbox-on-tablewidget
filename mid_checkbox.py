937271af807f71c0fd8c45e8698596c0c02521ec
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-10-07 19:12:00 +0700

    Update mid_checkbox.py

--- mid_checkbox.py
+++ mid_checkbox.py
@@ -1,44 +1,37 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+# ************************** man hinh loai 2 *************************
+import sys
+
+from PyQt5 import QtCore
+# pip install pyqt5
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QProxyStyle, QStyle
+from gui import Ui_MainWindow
 
 
-class CheckBoxStyle(QtWidgets.QProxyStyle):
+class CheckBoxStyle(QProxyStyle):
     def subElementRect(self, element, option, widget=None):
         r = super().subElementRect(element, option, widget)
-        if element == QtWidgets.QStyle.SE_ItemViewItemCheckIndicator:
+        if element == QStyle.SE_ItemViewItemCheckIndicator:
             r.moveCenter(option.rect.center())
         return r
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        data = ["1", "2", "3", "4", "5", "6"]
-
-        self.tableWidget = QtWidgets.QTableWidget(0, 2)
-        self.setCentralWidget(self.tableWidget)
-
-        for text in data:
-            row = self.tableWidget.rowCount()
-            self.tableWidget.insertRow(row)
-
-            text_item = QtWidgets.QTableWidgetItem(text)
-
-            checkbox_item = QtWidgets.QTableWidgetItem()
-            checkbox_item.setFlags(
-                QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled
-            )
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self)
+        self.uic.tableWidget.setRowCount(5)
+        self.uic.tableWidget.setColumnCount(3)
+        for row in range(5):
+            checkbox_item = QTableWidgetItem()
             checkbox_item.setCheckState(QtCore.Qt.Unchecked)
-
-            self.tableWidget.setItem(row, 0, text_item)
-            self.tableWidget.setItem(row, 1, checkbox_item)
+            self.uic.tableWidget.setItem(row, 0, checkbox_item)
 
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     checkbox_style = CheckBoxStyle(app.style())
     app.setStyle(checkbox_style)
-    w = MainWindow()
-    w.show()
-    sys.exit(app.exec_())
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
